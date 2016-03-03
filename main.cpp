2a0bf17e4fa78726a098ba9fc28ab100daf676d3
nils <nils@NB-Linux-Nils>
2016-03-03 11:58:32 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: nils
+ *
+ * Created on 3. März 2016, 11:58
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
